462e5df6f627216a1ebef46999a0d9594a02bed1
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-07-24 15:00:32 -0300

    15:00-07/24/2024: I added some comments.

--- Text_RPG_Cpp/Text_RPG_Cpp/src/states/GameMenuState.h
+++ Text_RPG_Cpp/Text_RPG_Cpp/src/states/GameMenuState.h
@@ -7,23 +7,27 @@ class Console;
 class StateMachine;
 class Keyboard;
 
+// GameMenuState class is a derived class of IState (a base class representing a state).
 class GameMenuState : public IState
 {
 private:
+	// References to Party, Console, StateMachine, and Keyboard objects.
 	Party& m_Party;
 	Console& m_Console;
 	StateMachine& m_StateMachine;
 	Keyboard& m_Keyboard;
 
 public:
+	// Constructor that initializes references to Party, Console, StateMachine, and Keyboard. Destructor
 	GameMenuState(Party& party, Console& console, StateMachine& stateMachine, Keyboard& keyboard);
 	~GameMenuState();
 
-	virtual void OnEnter() override;
-	virtual void OnExit() override;
-	virtual void Update() override;
-	virtual void Draw() override;
-	virtual void ProcessInputs() override;
+	// Virtual functions from IState that are overridden in GameMenuState.
+	virtual void OnEnter() override; // Called when entering the state.
+	virtual void OnExit() override; // Called when exiting the state.
+	virtual void Update() override; // Called every frame to update the state.
+	virtual void Draw() override; // Called every frame to draw the state.
+	virtual void ProcessInputs() override; // Called to process inputs in the state.
 
-	virtual bool Exit() override;
+	virtual bool Exit() override; // Called to exit the state.
 };
--- Text_RPG_Cpp/Text_RPG_Cpp/src/states/GameState.cpp
+++ Text_RPG_Cpp/Text_RPG_Cpp/src/states/GameState.cpp
@@ -153,6 +153,7 @@ void GameState::Draw()
 void GameState::ProcessInputs()
 {
 	// If the escape key was just pressed, pop the current state from the state machine.
+	// This will end the current game state and return to the previous state.
 	if (m_Keyboard.IsKeyJustPressed(KEY_ESCAPE))
 	{
 		m_StateMachine.PopState();
@@ -160,13 +161,16 @@ void GameState::ProcessInputs()
 		return;
 	}
 
-
+	// If the 'M' key was just pressed, push a new GameMenuState onto the state machine.
+	// This will pause the current game state and switch to the game menu state.
 	if (m_Keyboard.IsKeyJustPressed(KEY_M)) {
 		m_StateMachine.PushState(std::make_unique<GameMenuState>(*m_Party, m_Console, m_StateMachine, m_Keyboard));
 
 		return;
 	}
 
+	// Process inputs for the selector object.
+	// This could involve moving a cursor, making a selection, etc., depending on how the Selector class is defined.
 	m_Selector.ProcessInputs();
 }
 
