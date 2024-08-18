eb4aa45955cd433717aece9b4e035b2074d076d3
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-08-18 14:55:31 -0300

    14:54-08/18/2024: It was a mistake. I finished creating Typewriter at the previous commit. This one was the one I finished SetText.

--- Text_RPG_Cpp/Text_RPG_Cpp/src/utilities/Typewriter.cpp
+++ Text_RPG_Cpp/Text_RPG_Cpp/src/utilities/Typewriter.cpp
@@ -19,7 +19,6 @@ void Typewriter::ClearArea()
 
 Typewriter::Typewriter(Console& console) : Typewriter(console, 1, 1, L"TODO: Set type writer text!", 50, 250)
 {
-
 }
 
 Typewriter::Typewriter(Console& console, int start_x, int start_y, const std::wstring& text, int text_wrap, int speed, WORD textColor, WORD borderColor) :
@@ -59,7 +58,56 @@ Typewriter::~Typewriter()
 
 bool Typewriter::SetText(const std::wstring& text)
 {
-    return false;
+	m_sText = text;
+	m_sTextChunks.clear();
+
+	std::wstring text_holder = L"";
+
+	for (int i = 0; i < text.size(); i++)
+	{
+		text_holder += text[i];
+
+		bool newLine = text[i] == '\n';
+
+		size_t text_size = text_holder.size();
+
+		if (text_size >= m_TextWrap || newLine)
+		{
+			if (!newLine)
+			{
+				while (text[i] != ' ' && text[i] != '.' && text[i] != '!' && text[i] != '?' && text_size > 0)
+				{
+					i--;
+					text_holder.pop_back();
+				}
+			}
+			else
+			{
+				// Get rid of the new line character
+				text_holder.pop_back();
+			}
+
+			if (text_size > 0)
+			{
+				m_sTextChunks.push_back(text_holder);
+				text_holder.clear();
+			}
+		}
+	}
+
+	// Push in the rest of the text
+	if (!text_holder.empty())
+	{
+		m_sTextChunks.push_back(text_holder);
+	}
+
+	if (!SetBorderProperties())
+	{
+		TRPG_ERROR("Failed to set the border properties!");
+		return false;
+	}
+
+	return true;
 }
 
 void Typewriter::UpdateText()
