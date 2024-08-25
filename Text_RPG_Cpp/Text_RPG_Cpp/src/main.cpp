53e69b84221cc4b679592b329468d58e3b508566
tuantranquang0607 <tranquangtuan0607032gmail.com>
2024-08-25 16:05:12 -0300

    16:03-08/25/2024: I started to look into shops and the shop loader for the game. The shop loader would be based on the XML Parser that was created earlier.

--- Text_RPG_Cpp/Text_RPG_Cpp/src/main.cpp
+++ Text_RPG_Cpp/Text_RPG_Cpp/src/main.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 
-int main() {
+int main() 
+{
 	// Create an instance of the Game class.
 	Game game; 
 
--- Text_RPG_Cpp/Text_RPG_Cpp/src/utilities/trpg_utilities.h
+++ Text_RPG_Cpp/Text_RPG_Cpp/src/utilities/trpg_utilities.h
@@ -6,6 +6,7 @@
 
 #include "../Item.h"
 #include "../Equipment.h"
+#include "ShopParameters.h"
 
 // Template function to remove an element from a vector at a given index
 template <typename T> 
@@ -24,6 +25,9 @@ Item::ItemType ItemTypeFromString(const std::string& item_type);
 
 // Function declaration to convert a string to an EquipType enum.
 Equipment::EquipType EquipTypeFromString(const std::string& equip_type);
+
+// Function declaration to convert a string to a ShopType enum.
+ShopParameters::ShopType ShopTypeFromString(const std::string& shopType);
 
 // Function declaration to convert a string to a WeaponType enum.
 WeaponProperties::WeaponType WeaponTypeFromString(const std::string& weapon_type);
